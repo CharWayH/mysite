e8b0fd2606c8aa0d2c0c117d40f801a7a3eb7fab
CharWayH <2319155351@qq.com>
2018-11-27 14:31:02 +0800

    new

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from django.template import RequestContext, loader
+from .models import Question
 
 # Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('id')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'polls/detail.html',{'question':question})
+    #return HttpResponse("You're looking at question %s."%question_id)
+    #question_id= Question.objects.
+    #context = {'question_id':question_id}
+    #return render(request,'polls/detail.html',question_id)
+
+def result(request,question_id):
+    return HttpResponse("You're looking at the results of question %s."%question_id)
+
+def vote(request,question_id):
+    return HttpResponse("Your're voting on question %s."%question_id)
